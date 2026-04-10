083b8b327051ee95e2dd3bcb08e248ccba76844d
user0 <danielleaman03@gmail.com>
2026-04-10 16:11:38 -0400

    functions work

--- test/dltb.php
+++ test/dltb.php
@@ -2,7 +2,9 @@
 include_once("../database/dbpersonhours.php");
 include_once("../database/dbRoles.php");
 include_once("../database/dbRoleEvents.php");
+include_once("../database/reports.php");
 
 
-$r = calcTop10();
+$r = hoursPerRoleAllTime([100,2,3,4,5]);
 var_dump($r);
+
