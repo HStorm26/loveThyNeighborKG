eac7190b5908c0680d01e19b62ad9670865a53f4
oscarSheeheclear <osheehe@mail.umw.edu>
2026-04-18 18:17:24 -0400

    pdf option for top10VolunteersReport

--- topVolunteersReport.php
+++ topVolunteersReport.php
@@ -26,25 +26,25 @@ $fiscalYearEnd = $fiscalYearStart + 1;
     <title>Top Volunteer Report | Love Thy Neighbor Community Food Pantry</title>
     <!--<script src="js/data-filters.js" defer></script>-->
     <!-- <link href="css/base.css" rel="stylesheet"> -->
-     <link href="layoutInfo.css" rel="stylesheet">
+    <link href="layoutInfo.css" rel="stylesheet">
     <?php require_once('header.php'); ?>
 </head>
 <body>
-    <?php require_once('database/dbEvents.php');?>
-    <?php require_once('database/dbPersons.php');?>
+    <?php require_once('database/dbEvents.php'); ?>
+    <?php require_once('database/dbPersons.php'); ?>
+
     <div class="page-wrapper">
         <div class="info-card">
-        <!-- Hero Section with Title and Info-->
             <div class="info-header">
                 <h1>Generate Top Volunteers Report</h1>
-                <p>Use this tool to generate a list of the top ten volunteers by total hours. Reports are available in Excel or CSV format</p>
+                <p>Use this tool to generate a list of the top ten volunteers by total hours. Reports are available in PDF, Excel, or CSV format.</p>
             </div>
 
             <form method="POST" action="processTop10Report.php" class="info-form">
-                <!-- Format -->
                 <div style="margin-bottom: 1.5rem; margin-top: 1.5rem;">
                     <label for="format" style="font-weight: 600;">File Format</label>
                     <select name="format" id="format">
+                        <option value="pdf">PDF (.pdf)</option>
                         <option value="excel">Excel (.xls)</option>
                         <option value="csv">CSV (.csv)</option>
                     </select>
@@ -52,17 +52,13 @@ $fiscalYearEnd = $fiscalYearStart + 1;
 
                 <div class="email-actions">
                     <input type="hidden" value="<?php echo $_SESSION['_id']; ?>" name="admin" id="admin">
-                    <input type="hidden" value="<?php echo date("d-M-Y H:i:s e") ?>" name="time" id="time">
+                    <input type="hidden" value="<?php echo date("d-M-Y H:i:s e"); ?>" name="time" id="time">
                     <button type="submit" name="action" value="send" class="submit-btn">Generate Report</button>
                 </div>
             </form>
-
         </div>
     </div>
 
-    </main>
     <?php include 'footer.php'; ?>
-
-   
 </body>
 </html>
